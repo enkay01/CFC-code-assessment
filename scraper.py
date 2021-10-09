a9ad0dc8c83708e8f20af7eb068c81fb336540fc
Ian <psyin@exmail.nottingham.ac.uk>
2021-10-09 23:05:02 +0100

    Added function to get text from web page and sanitize. Added function to count unique words in list. Created json of word counts.

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 from requests import get
+from collections import Counter
 from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
 from contextlib import closing
@@ -7,6 +8,7 @@ import json
 
 url = 'http://www.cfcunderwriting.com'
 priv_pol_link = None
+HTML_PARSER = 'html5lib'
 
 def simple_get(url):
     try:
@@ -21,31 +23,31 @@ def simple_get(url):
         return None
 
 def is_good_response(resp):
-    "Returns true if respons contains HTML"
+    #Returns true if respons contains HTML
     
     content_type = resp.headers['Content-Type'].lower()
     return (resp.status_code == 200 and content_type is not None and content_type.find('html') > -1)
 
-def write_to_json(data):
-    with open('external resources.json', 'w', encoding='utf-8') as f:
+def write_to_json(data, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=True, indent=4)
 
+#Gets head of web page. Finds all links. Returns string list of links
 def get_external_resources(url):
     raw_html = simple_get(url)
-    html = BeautifulSoup(raw_html, 'html.parser')
+    html = BeautifulSoup(raw_html, HTML_PARSER)
     head = html.head
     links = head.find_all('link')
     external_res = []
     for x in links:
-        if x.name == 'link':
-            if "cfcunderwriting.com" not in x.get('href'):
-                external_res.append(x.get('href'))
+        if x.name == 'link' and "cfcunderwriting.com" not in x.get('href'):
+            external_res.append(x.get('href'))
 
     return external_res
-
+#Gets body of web page Finds all links. Returns string list of links
 def find_all_links(url):
     raw_html = simple_get(url)
-    html = BeautifulSoup(raw_html, 'html.parser')
+    html = BeautifulSoup(raw_html, HTML_PARSER)
     body = html.body
     links = body.find_all('a')
     hyperlinks = []
@@ -53,18 +55,28 @@ def find_all_links(url):
         if not '#' in l.get('href', []):
             hyperlinks.append(l.get('href', []))
     return hyperlinks
-
+#Takes main element of page. Removes whitespace, punctuation, and numbers. Returns list of words.
 def scrape_page_text(url):
     raw_html = simple_get(url)
-    html = BeautifulSoup(raw_html, 'html.parser')
-        
+    html = BeautifulSoup(raw_html, HTML_PARSER)
+    main = html.find('main')
+    text = main.text.strip()
+    text = ''.join(e for e in text if e.isalnum() or " " in e)
+    words = text.split(" ")
+     
+    return words
+#Returns dictionary of words with counts
+def count_words(word_list):
+    return Counter(word_list)
 
 if __name__ == '__main__':
-    #external_res = get_external_resources(url)
-    #write_to_json(external_res)
+    external_res = get_external_resources(url)
+    write_to_json(external_res, 'external resources.json')
     links = find_all_links(url)
     
     for l in links: 
         if "privacy-policy" in l:
             priv_pol_link = url + l
-    
+    main = scrape_page_text(priv_pol_link)   
+    counts = count_words(main)
+    write_to_json(counts, 'word counts.json')

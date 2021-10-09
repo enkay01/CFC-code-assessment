324ba7f135a51a0bd28124437eccae93838c962a
Ian <psyin@exmail.nottingham.ac.uk>
2021-10-09 15:12:09 +0100

    Added functionality to enumerate links from page Added functionality to get privacy policy link

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ import json
 
 
 url = 'http://www.cfcunderwriting.com'
+priv_pol_link = None
 
 def simple_get(url):
     try:
@@ -29,15 +30,41 @@ def write_to_json(data):
     with open('external resources.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=True, indent=4)
 
+def get_external_resources(url):
+    raw_html = simple_get(url)
+    html = BeautifulSoup(raw_html, 'html.parser')
+    head = html.head
+    links = head.find_all('link')
+    external_res = []
+    for x in links:
+        if x.name == 'link':
+            if "cfcunderwriting.com" not in x.get('href'):
+                external_res.append(x.get('href'))
 
-raw_html = simple_get(url)
-html = BeautifulSoup(raw_html, 'html.parser')
-head = html.head
-links = head.find_all('link')
-external_res = []
-for x in links:
-    if x.name == 'link':
-        if "cfcunderwriting.com" not in x.get('href'):
-            external_res.append(x.get('href'))
+    return external_res
 
-write_to_json(external_res)
+def find_all_links(url):
+    raw_html = simple_get(url)
+    html = BeautifulSoup(raw_html, 'html.parser')
+    body = html.body
+    links = body.find_all('a')
+    hyperlinks = []
+    for l in links:
+        if not '#' in l.get('href', []):
+            hyperlinks.append(l.get('href', []))
+    return hyperlinks
+
+def scrape_page_text(url):
+    raw_html = simple_get(url)
+    html = BeautifulSoup(raw_html, 'html.parser')
+        
+
+if __name__ == '__main__':
+    #external_res = get_external_resources(url)
+    #write_to_json(external_res)
+    links = find_all_links(url)
+    
+    for l in links: 
+        if "privacy-policy" in l:
+            priv_pol_link = url + l
+    
